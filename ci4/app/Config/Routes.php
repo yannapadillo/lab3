8d794b5a81f04413fff59a1da9aabe6c5971d0cb
yannapadillo <lzpadillo@student.apc.edu.ph>
2023-03-03 17:56:15 +0800

    game

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -42,8 +42,7 @@ $routes->get('guests', [Guests::class, 'index']);
 $routes->match(['get', 'post'], 'news/create', [News::class, 'create']);
 $routes->get('news/(:segment)', [News::class, 'view']);
 $routes->get('news', [News::class, 'index']);
-$routes->get('games/(:segment)', [Games::class, 'view']);
-$routes->get('games', [Games::class, 'index']);
+$routes->get('games', 'Games::index');
 $routes->get('pages', [Pages::class, 'index']);
 $routes->get('(:segment)', [Pages::class, 'view']);
 
