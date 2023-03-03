e344d362b0aae1de6ab145f79658f1babe5590ed
yannapadillo <lzpadillo@student.apc.edu.ph>
2023-03-03 18:56:19 +0800

    index

--- ci4/app/Views/game/index.php
+++ ci4/app/Views/game/index.php
@@ -5,6 +5,7 @@
     <title>Lyanna Padillo</title>
     <link href="img/h1.png" rel="icon" type = "image/x-icon">
     <link rel="stylesheet" href="css/snake.css">
+    <link rel="stylesheet" href="css/styles.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
     <script src="js/snake.js" defer></script>
