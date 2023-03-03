9585b67e092539c7a50af50c130fbefaf71debc3
yannapadillo <lzpadillo@student.apc.edu.ph>
2023-03-03 17:18:45 +0800

    routes

---
+++ ci4/app/Controllers/Games.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Exceptions\PageNotFoundException; // Add this line
+
+class Games extends BaseController
+{
+    public function index()
+    {
+        return view('game');
+    }
+
+}
