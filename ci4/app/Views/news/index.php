8dead05f7d92d2604f8e94d115a6e6810991993f
yannapadillo <lzpadillo@student.apc.edu.ph>
2023-03-03 13:29:14 +0800

    news

--- ci4/app/Views/news/index.php
+++ ci4/app/Views/news/index.php
@@ -26,7 +26,7 @@
 
         <h3><?= esc($news_item['title']) ?></h3>
 
-        <div class="main">
+        <div class="body">
             <?= esc($news_item['body']) ?>
         </div>
         <p><a href="/~lzpadillo/lab3/ci4/public/news/<?= esc($news_item['slug'], 'url') ?>">View article</a></p>
@@ -40,28 +40,6 @@
     <p>Unable to find any news for you.</p>
 
 <?php endif ?>
-<footer class="footer section" id="contact">
-    <div class="footer-social">
-      <a href="https://www.linkedin.com/in/lyanna-padillo-8ba1b321a/" target="_blank" class="footer-icon"><i
-          class="bx bxl-linkedin">
-          <br />
-          LinkedIn</i></a>
-      <a href="https://twitter.com/LiliArts__" target="_blank" class="footer-icon"><i class="bx bxl-twitter">
-          <br />
-          Twitter</i></a>
-      <a href="mailto:lzpadillo@student.apc.edu.ph" target="_blank" class="footer-icon"><i class="bx bx-mail-send">
-          <br />
-          E-mail</i></a>
-      <a href="tel:+63 9771135400" target="_blank" class="footer-icon"><i class="bx bx-phone">
-          <br />
-          Phone</i></a>
-      <a href="https://github.com/yannapadillo" target="_blank" class="footer-icon"><i
-            class="bx bxl-github">
-          <br />
-          GitHub
-          </i></a>
-    </div>
-    <p>&#169; 2022 copyright all right reserved</p>
-  </footer>
+
 </body>
 </html>
