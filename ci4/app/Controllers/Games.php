78b75239a85a6c7220f938e81cfa534f8e58115c
yannapadillo <lzpadillo@student.apc.edu.ph>
2023-03-03 17:20:59 +0800

    revision routes

--- ci4/app/Controllers/Games.php
+++ ci4/app/Controllers/Games.php
@@ -11,4 +11,16 @@ class Games extends BaseController
         return view('game');
     }
 
+    public function view($page = 'index')
+    {
+        if (! is_file(APPPATH . 'Views/game/' . $page . '.php')) {
+            // Whoops, we don't have a page for that!
+            throw new PageNotFoundException($page);
+        }
+
+        $data['title'] = ucfirst($page); // Capitalize the first letter
+
+        return view('templates/header', $data)
+            . view('pages/' . $page)
+    }
 }
